c5d693ea1f09da8c5db24348cf15f3e75858c71d
Dang Khai Nghi <nghidk@ebizworld.com.vn>
2025-02-25 21:17:40 +0700

    3rd

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
                     <i className="fa-solid fa-bars" onClick={toggleNavbar}></i>
                 </div>
                 <div className={isNavbarOpen ? "block sm:hidden mt-4" : "hidden sm:hidden mt-4"}>
-                    <a href="#" className="block text-center bg-blue-400 rounded-lg m-6 text-white mb-2" onClick={toggleNavbar}>Home</a>
+                    <a href="/" className="block text-center bg-blue-400 rounded-lg m-6 text-white mb-2" onClick={toggleNavbar}>Home</a>
                     <a href="#about" className="block text-center bg-blue-400 rounded-lg m-6 text-white mb-2" onClick={toggleNavbar}>About</a>
                     <a href="#portfolio" className="block text-center bg-blue-400 rounded-lg m-6 text-white mb-2" onClick={toggleNavbar}>Portfolio</a>
                     <a href="#contact" className="block text-center bg-blue-400 rounded-lg m-6 text-white" onClick={toggleNavbar}>Contact</a>
